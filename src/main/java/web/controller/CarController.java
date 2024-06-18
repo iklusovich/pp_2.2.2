49e28589581689742e5adc9ab9d3e1fcbf663703
i.klusovich <ilya.klusovich.89@mail.ru>
2024-06-18 19:14:24 +0300

    fix

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -23,10 +23,7 @@ public class CarController {
     @GetMapping(value = "/cars")
 
     public String getCars( @RequestParam(value = "count", defaultValue = "5") int count, ModelMap model) {
-        if (count > 5) {
-            count = 5;
-        }
-        model.addAttribute("cars", carService.getCars().stream().limit(count).toList());
+        model.addAttribute("cars", carService.getCars().stream().limit(count > 5 ? 5 : count).toList());
         model.addAttribute("title", "Cars list");
         return "cars";
     }
