3e62bd888a1cef48b86f04fb19a7eef73ad6ba7f
i.klusovich <ilya.klusovich.89@mail.ru>
2024-06-18 16:24:21 +0300

    done

---
+++ src/main/java/web/model/Car.java
@@ -0,0 +1,39 @@
+package web.model;
+
+public class Car {
+    String model;
+    int series;
+    String color;
+
+    public Car() {
+
+    }
+
+    public Car(String model, int series, String color) {
+        this.model = model;
+        this.series = series;
+        this.color = color;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public int getSeries() {
+        return series;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public void setSeries(int series) {
+        this.series = series;
+    }
+
+    public void setColor(String color) {}
+}
